2f7894a76dbf5fcd568070d22740106e54907279
leeenah <76856144+leeenah@users.noreply.github.com>
2021-03-17 21:54:57 -0700

    Removed City component and added the code in it to the Temperature component. Added API integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import Search from "./Search";
-import City from "./City";
 import Temperature from "./Temperature";
 import Forecast from "./Forecast";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
-import "./City.css";
 
 export default function App() {
   return (
     <div className="App">
       <Search />
-      <City />
-      <Temperature />
+      <Temperature city="Auckland" />
       <Forecast color="black" size={40} />
       <footer>
         {" "}
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Temperature.css";
 
-export default function Temperature() {
-  let weatherData = {
-    temperature: 10,
-    description: "clear",
-    feelsLike: 2,
-    humidity: 70,
-  };
+export default function Temperature(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
 
-  return (
-    <div className="Temperature">
-      <span className="current-temperature"> {weatherData.temperature} </span>
-      {/* removed className = "active" */}
-      <span className="celcius-link">°C |</span>
+  function getResponse(response) {
+    setWeatherData({
+      city: response.data.name,
+      date: "Wednesday 10:00",
+      temperature: response.data.main.temp,
+      feelsLike: response.data.main.feels_like,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].main,
+    });
+    setReady(true);
+  }
 
-      <span className="farenheit-link">°F</span>
-      <h3> {weatherData.description} </h3>
-      <div className="row">
-        <div className="col-sm">
-          Feels like: <span> {weatherData.feelsLike} </span>°C | Humidity:
-          <span> {weatherData.humidity} </span> %
+  if (ready) {
+    return (
+      <div className="Temperature">
+        <h1> {weatherData.city} </h1>
+        <p className="currentDay"> {weatherData.date}</p>
+        <p className="currently">currently</p>
+        <span className="current-temperature">
+          {" "}
+          {Math.round(weatherData.temperature)}{" "}
+        </span>
+        {/* removed className = "active" */}
+        <span className="celcius-link">°C |</span>
+
+        <span className="farenheit-link">°F</span>
+        <h3 class="text-lowercase"> {weatherData.description} </h3>
+        <div className="row">
+          <div className="col-sm">
+            Feels like: <span> {Math.round(weatherData.feelsLike)} </span>°C |
+            Humidity:
+            <span> {Math.round(weatherData.humidity)} </span> %
+          </div>
         </div>
+        <hr />
       </div>
-      <hr />
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "fc744c97c485c14d19b2746947729882";
+    let unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(getResponse);
+
+    return "searching...";
+  }
 }
